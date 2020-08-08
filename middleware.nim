0a58f52f9001879cd5353c172b53e25fd772fe4a
flywind <xzsflywind@gmail.com>
2020-08-08 17:20:31 +0800

    Deployed 16c1f62 with MkDocs version: 1.0.4

---
+++ middleware.nim
@@ -0,0 +1,22 @@
+import logging
+import prologue
+
+
+proc hello(ctx: Context) {.async.} =
+  discard
+
+proc myDebugRequestMiddleware*(appName = "Starlight"): HandlerAsync =
+  result = proc(ctx: Context) {.async.} =
+    logging.info "debugRequestMiddleware->begin"
+    # do something before
+    await switch(ctx)
+    # do something after
+    logging.info "debugRequestMiddleware->End"
+
+
+var
+  settings = newSettings()
+  app = newApp(settings = settings, middlewares = @[myDebugRequestMiddleware()])
+
+
+app.addRoute("/", hello, HttpGet, middlewares = @[myDebugRequestMiddleware()])
